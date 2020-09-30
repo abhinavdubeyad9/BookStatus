0d21415cde1b5ebe5647ee7e925d4bfc8b9b788c
Rubal Agrawal <59495047+Rubal-MB@users.noreply.github.com>
2020-09-30 10:27:49 -0700

    initial coomit

--- insert.py
+++ insert.py
@@ -9,16 +9,21 @@ if curbook.fetchone()[0]!=1 :
     bookID INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     author STRING NOT NULL,
-    price FLOAT NOT NULL)''')
+    price FLOAT NOT NULL,
+    YOP INTEGER NOT NULL,
+    Lang STRING NOT NULL)''')
 
 
 bID=int(input("enter book ID:"))
 bookname=input("enter name of the book:")
 bookauth=input("enter name of the author:")
 bookprice=float(input("enter price of the book:"))
+YearOfPublication=int(input("enter year of publication"))
+suitablelanguage=string(input("enter the type of language you want")
+
 
 try:
-    curbook.execute("INSERT INTO books(bookID,title,author,price) VALUES(?,?,?,?)", (bID,bookname,bookauth,bookprice))
+    curbook.execute("INSERT INTO books(bookID,title,author,price,YOP,Lang) VALUES(?,?,?,?)", (bID,bookname,bookauth,bookprice,YearOfPublication,suitablelanguage))
     Mybooks.commit()
     print("book added successfully")
 except:
